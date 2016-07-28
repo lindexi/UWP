b13506dd4f3b555e9624c138e36e1a59528a6764
lindexi_gd <lindexi_gd@163.com>
2016-07-28 15:28:59 +0800

    切换主题

---
+++ uwp/control/NightDayThemeToggleButton/NightDayThemeToggleButton/ViewModel/ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace NightDayThemeToggleButton.ViewModel
+{
+    public class ViewModel : NotifyProperty
+    {
+        public ViewModel()
+        {
+        }
+
+        public ElementTheme Theme
+        {
+            get
+            {
+                return _theme;
+            }
+            set
+            {
+                _theme = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ElementTheme _theme = ElementTheme.Light;
+    }
+}

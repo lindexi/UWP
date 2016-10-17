cb94f3bd605e6fd71bef8199929a70aa33a27339
lindexi_gd <lindexi_gd@163.com>
2016-10-17 21:40:41 +0800

    兴趣线

---
+++ uwp/control/InteresRoller/InteresRoller/ViewModel/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteresRoller.ViewModel
+{
+    public class ViewModel : NotifyProperty
+    {
+        public ViewModel()
+        {
+
+        }
+
+        public ObservableCollection<string> Str
+        {
+            set;
+            get;
+        }=new ObservableCollection<string>()
+        {
+            "林德熙",
+            "http://blog.csdn.net/lindexi_gd",
+            "http://github.com/lindexi"
+        };
+    }
+}

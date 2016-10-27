28692a9b2e70375c7be854a3da2872e4e7e14bb2
lindexi_gd <lindexi_gd@163.com>
2016-10-27 14:04:55 +0800

    定义在使用 自动属性

--- uwp/control/BitStamp/BitStamp/Model/clouddn/IO/IOClient.cs
+++ uwp/control/BitStamp/BitStamp/Model/clouddn/IO/IOClient.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// lindexi
+// 15:52
+
+using System;
 using System.Collections.Generic;
 using System.Net;
 using Qiniu.Conf;
@@ -12,142 +15,116 @@ using Windows.Storage;
 using Windows.Web.Http;
 using Qiniu.Auth.digest;
 using Qiniu.RS;
+using HttpStatusCode = System.Net.HttpStatusCode;
 
 namespace Qiniu.IO
 {
     /// <summary>
-    /// 上传客户端
+    ///     上传客户端
     /// </summary>
     public class IOClient
     {
-        /// <summary>
-        /// 无论成功或失败，上传结束时触发的事件
-        /// </summary>
-        public event EventHandler<PutRet> PutFinished;
-
-        private static WebHeaderCollection /*NameValueCollection*/ getFormData(string upToken, string key, PutExtra extra)
+        public IOClient()
         {
-            //System.Collections.Specialized
-            WebHeaderCollection webHeader=new WebHeaderCollection();
-            //NameValueCollection formData = new NameValueCollection();
-            webHeader["token"] = upToken;
-            //formData["token"] = upToken;
-
-            if (key!=null)
-            {
-                webHeader["key"] = key;
-                //formData["key"] = key;
-            }
-            if (extra != null)
-            {
-                if (extra.CheckCrc == CheckCrcType.CHECK_AUTO)
-                {
-                    webHeader["crc32"]= extra.Crc32.ToString();
-                    //formData["crc32"] = extra.Crc32.ToString();
-                }
-                if (extra.Params != null)
-                {
-                    foreach (KeyValuePair<string, string> pair in extra.Params)
-                    {
-                        webHeader[pair.Key] = pair.Value;
-                        //formData[pair.Key] = pair.Value;
-                    }
-                }
-            }
-            return webHeader;
-            //return formData;
         }
 
         /// <summary>
-        /// 设置连接代理
+        ///     设置连接代理
         /// </summary>
-        public IWebProxy Proxy { get; set; }
-
-
-		/// <summary>
-		/// 上传文件
-		/// </summary>
-		/// <param name="upToken"></param>
-		/// <param name="key"></param>h
-		/// <param name="localFile"></param>
-		/// <param name="extra"></param>
-		public async System.Threading.Tasks.Task<PutRet> PutFile(string upToken, string key, string localFile, PutExtra extra)
-		{
-			if (!System.IO.File.Exists(localFile))
-			{
-				throw new Exception(string.Format("{0} does not exist", localFile));
-			}
-			PutRet ret;
-
-            /*NameValueCollection*/
-            WebHeaderCollection formData = getFormData(upToken, key, extra);
-			try
-			{
-				CallRet callRet = await MultiPart.MultiPost(Config.UP_HOST, formData, localFile, this.Proxy);
-				ret = new PutRet(callRet);
-				onPutFinished(ret);
-				return ret;
-			}
-			catch (Exception e)
-			{
-				ret = new PutRet(new CallRet(System.Net.HttpStatusCode.BadRequest, e));
-				onPutFinished(ret);
-				return ret;
-			}
-		}
-		/// <summary>
-		/// Puts the file without key.
-		/// </summary>
-		/// <returns>The file without key.</returns>
-		/// <param name="upToken">Up token.</param>
-		/// <param name="localFile">Local file.</param>
-		/// <param name="extra">Extra.</param>
-		public async System.Threading.Tasks.Task<PutRet> PutFileWithoutKey(string upToken, string localFile, PutExtra extra)
-		{
-			return await PutFile(upToken, null, localFile, extra);
-		}
-
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="upToken">Up token.</param>
-		/// <param name="key">Key.</param>
-		/// <param name="putStream">Put stream.</param>
-		/// <param name="extra">Extra.</param>
-		public async System.Threading.Tasks.Task<PutRet> Put(string upToken, string key, System.IO.Stream putStream, PutExtra extra)
-		{
-			if (!putStream.CanRead)
-			{
-				throw new Exception("read put Stream error");
-			}
-			PutRet ret;
-            /*NameValueCollection*/
-            WebHeaderCollection formData = getFormData(upToken, key, extra);
-			try
-			{
-				CallRet callRet = await MultiPart.MultiPost(Config.UP_HOST, formData, putStream);
-				ret = new PutRet(callRet);
-				onPutFinished(ret);
-				return ret;
-			}
-			catch (Exception e)
-			{
-				ret = new PutRet(new CallRet(System.Net.HttpStatusCode.BadRequest, e));
-				onPutFinished(ret);
-				return ret;
-			}
-		}
-
-		protected void onPutFinished(PutRet ret)
+        public IWebProxy Proxy
         {
-            if (PutFinished != null)
+            set;
+            get;
+        }
+
+        /// <summary>
+        ///     无论成功或失败，上传结束时触发的事件
+        /// </summary>
+        public event EventHandler<PutRet> OnPutFinished;
+
+
+        /// <summary>
+        ///     上传文件
+        /// </summary>
+        /// <param name="upToken"></param>
+        /// <param name="key"></param>
+        /// h
+        /// <param name="localFile"></param>
+        /// <param name="extra"></param>
+        public async Task<PutRet> PutFile(string upToken, string key, string localFile, PutExtra extra)
+        {
+            if (!File.Exists(localFile))
             {
-                PutFinished(this, ret);
+                throw new Exception(string.Format("{0} does not exist", localFile));
+            }
+            //PutRet ret;
+
+            /*NameValueCollection*/
+            WebHeaderCollection formData = GetFormData(upToken, key, extra);
+            try
+            {
+                CallRet callRet = await MultiPart.MultiPost(Config.UP_HOST, formData, localFile, this.Proxy);
+                var ret = new PutRet(callRet);
+                PutFinished(ret);
+                return ret;
+            }
+            catch (Exception e)
+            {
+                var ret = new PutRet(new CallRet(HttpStatusCode.BadRequest, e));
+                PutFinished(ret);
+                return ret;
             }
         }
 
         /// <summary>
-        /// 上传文件
+        ///     Puts the file without key.
+        /// </summary>
+        /// <returns>The file without key.</returns>
+        /// <param name="upToken">Up token.</param>
+        /// <param name="localFile">Local file.</param>
+        /// <param name="extra">Extra.</param>
+        public async Task<PutRet> PutFileWithoutKey(string upToken, string localFile, PutExtra extra)
+        {
+            return await PutFile(upToken, null, localFile, extra);
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="upToken">Up token.</param>
+        /// <param name="key">Key.</param>
+        /// <param name="putStream">Put stream.</param>
+        /// <param name="extra">Extra.</param>
+        public async Task<PutRet> Put(string upToken, string key, Stream putStream, PutExtra extra)
+        {
+            if (!putStream.CanRead)
+            {
+                throw new Exception("read put Stream error");
+            }
+            //PutRet ret;
+            /*NameValueCollection*/
+            WebHeaderCollection formData = GetFormData(upToken, key, extra);
+            try
+            {
+                CallRet callRet = await MultiPart.MultiPost(Config.UP_HOST, formData, putStream);
+                var ret = new PutRet(callRet);
+                PutFinished(ret);
+                return ret;
+            }
+            catch (Exception e)
+            {
+                var ret = new PutRet(new CallRet(HttpStatusCode.BadRequest, e));
+                PutFinished(ret);
+                return ret;
+            }
+        }
+
+        protected void PutFinished(PutRet ret)
+        {
+            OnPutFinished?.Invoke(this, ret);
+        }
+
+        /// <summary>
+        ///     上传文件
         /// </summary>
         /// <param name="accessKey"></param>
         /// <param name="secretKey"></param>
@@ -161,7 +138,7 @@ namespace Qiniu.IO
             StorageFile file,
             string name = null)
         {
-            Mac mac = new Mac(accessKey, Qiniu.Conf.Config.Encoding.GetBytes(secretKey));
+            Mac mac = new Mac(accessKey, Config.Encoding.GetBytes(secretKey));
             PutPolicy putPolicy = new PutPolicy();
             putPolicy.Scope = bucket;
             putPolicy.SetExpires(3600);
@@ -170,11 +147,48 @@ namespace Qiniu.IO
 
             Stream stream = await file.OpenStreamForReadAsync();
 
-            var ret= await new Qiniu.IO.IOClient().Put(uploadToken, name, stream, new PutExtra());
+            var ret = await new IOClient().Put(uploadToken, name, stream, new PutExtra());
 
             stream.Dispose();
 
             return ret;
+        }
+
+        private static WebHeaderCollection /*NameValueCollection*/ GetFormData(string upToken,
+            string key,
+            PutExtra extra)
+        {
+            //System.Collections.Specialized
+            WebHeaderCollection webHeader = new WebHeaderCollection
+            {
+                ["token"] = upToken
+            };
+            //NameValueCollection formData = new NameValueCollection();
+            //formData["token"] = upToken;
+
+            if (key != null)
+            {
+                webHeader["key"] = key;
+                //formData["key"] = key;
+            }
+            if (extra != null)
+            {
+                if (extra.CheckCrc == CheckCrcType.CHECK_AUTO)
+                {
+                    webHeader["crc32"] = extra.Crc32.ToString();
+                    //formData["crc32"] = extra.Crc32.ToString();
+                }
+                if (extra.Params != null)
+                {
+                    foreach (KeyValuePair<string, string> pair in extra.Params)
+                    {
+                        webHeader[pair.Key] = pair.Value;
+                        //formData[pair.Key] = pair.Value;
+                    }
+                }
+            }
+            return webHeader;
+            //return formData;
         }
     }
 }

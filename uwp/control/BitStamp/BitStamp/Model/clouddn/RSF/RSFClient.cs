5740e3024c0aa446aa4ac44ca20abc1fcf427431
lindexi_gd <lindexi_gd@163.com>
2016-10-30 12:39:33 +0800

    排列属性，修改字段名，修改参数名

--- uwp/control/BitStamp/BitStamp/Model/clouddn/RSF/RSFClient.cs
+++ uwp/control/BitStamp/BitStamp/Model/clouddn/RSF/RSFClient.cs
@@ -1,5 +1,9 @@
+// lindexi
+// 15:59
+
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Qiniu.Auth;
 using Qiniu.Conf;
@@ -7,174 +11,172 @@ using Qiniu.RPC;
 
 namespace Qiniu.RSF
 {
-	/// <summary>
-	/// RS Fetch 
-	/// </summary>
-	public class RSFClient : QiniuAuthClient
-	{        
-		private const int MAX_LIMIT = 1000;
-		//失败重试次数
-		private const int RETRY_TIME = 3;
-		private string bucketName;
+    /// <summary>
+    ///     RS Fetch
+    /// </summary>
+    public class RSFClient : QiniuAuthClient
+    {
+        /// <summary>
+        ///     RS Fetch Client
+        /// </summary>
+        /// <param name="bucketName">七牛云存储空间名称</param>
+        public RSFClient(string bucketName)
+        {
+            this._bucketName = bucketName;
+        }
 
-		/// <summary>
-		/// bucket name
-		/// </summary>
-		public string BucketName { get; private set; }
+        private const int MaxLimit = 1000;
+        //失败重试次数
+        private const int RetryTime = 3;
 
-		private int limit;
+        /// <summary>
+        ///     bucket name
+        /// </summary>
+        public string BucketName
+        {
+            get;
+            private set;
+        }
 
-		/// <summary>
-		/// Fetch返回结果条目数量限制
-		/// </summary>
-		public int Limit {
-			get {
-				return limit;
-			}
-			set {
-				limit = value > MAX_LIMIT ? MAX_LIMIT : value;
-			}
-		}
+        /// <summary>
+        ///     Fetch返回结果条目数量限制
+        /// </summary>
+        public int Limit
+        {
+            get
+            {
+                return _limit;
+            }
+            set
+            {
+                _limit = value > MaxLimit ? MaxLimit : value;
+            }
+        }
 
-		private bool end = false;
-		private string prefix;
+        /// <summary>
+        ///     文件前缀
+        /// </summary>
+        /// <value>
+        ///     The prefix.
+        /// </value>
+        public string Prefix
+        {
+            set;
+            get;
+        }
 
-		/// <summary>
-		/// 文件前缀
-		/// </summary>
-		/// <value>
-		/// The prefix.
-		/// </value>
-		public string Prefix {
-			get {
-				return prefix;
-			}
-			set {
-				prefix = value;
-			}
-		}
+        /// <summary>
+        ///     Fetch 定位符.
+        /// </summary>
+        public string Marker
+        {
+            set;
+            get;
+        }
 
-		private string marker;
+        /// <summary>
+        ///     The origin Fetch interface,we recomment to use Next().
+        /// </summary>
+        /// <returns>
+        ///     Dump
+        /// </returns>
+        /// <param name='bucket'>
+        ///     Bucket name.
+        /// </param>
+        /// <param name='prefix'>
+        ///     Prefix.
+        /// </param>
+        /// <param name='markerIn'>
+        ///     Marker in.
+        /// </param>
+        /// <param name='limitFetch'>
+        ///     Limit.
+        /// </param>
+        public async Task<DumpRet> ListPrefix(string bucket, 
+            string prefix = "", 
+            string markerIn = "", 
+            int limitFetch = 0)
+        {
+            string url = Config.RSF_HOST + 
+                string.Format("/list?bucket={0}", bucket); // + bucketName + 
+            if (!string.IsNullOrEmpty(markerIn))
+            {
+                url += string.Format("&marker={0}", markerIn);
+            }
+            if (!string.IsNullOrEmpty(prefix))
+            {
+                url += string.Format("&prefix={0}", prefix);
+            }
+            if (limitFetch > 0)
+            {
+                url += string.Format("&limit={0}", limitFetch);
+            }
+            for (int i = 0; i < RetryTime; i++)
+            {
+                CallRet ret = await Call(url);
+                if (ret.OK)
+                {
+                    return JsonConvert.DeserializeObject<DumpRet>(ret.Response);
+                }
+            }
+            return null;
+        }
 
-		/// <summary>
-		/// Fetch 定位符.
-		/// </summary>
-		public string Marker {
-			get {
-				return marker;
-			}
-			set {
-				marker = value;
-			}
-		}
+        /// <summary>
+        ///     call this func before invoke Next()
+        /// </summary>
+        public void Init()
+        {
+            _end = false;
+            Marker = string.Empty;
+        }
 
-		/// <summary>
-		/// RS Fetch Client
-		/// </summary>
-		/// <param name="bucketName">七牛云存储空间名称</param>
-		public RSFClient (string bucketName)
-		{
-			this.bucketName = bucketName;
-		}
+        /// <summary>
+        ///     Next.
+        ///     <example>
+        ///         <code>
+        ///  public static void List (string bucket)
+        /// {
+        ///      RSF.RSFClient rsf = new Qiniu.RSF.RSFClient(bucket);
+        ///      rsf.Prefix = "test";
+        ///      rsf.Limit = 100;
+        ///      List<DumpItem>
+        ///                 items;
+        ///                 while ((items=rsf.Next())!=null)
+        ///                 {
+        ///                 //todo
+        ///                 }
+        ///                 }s
+        ///  </code>
+        ///     </example>
+        /// </summary>
+        public async Task<List<DumpItem>> Next()
+        {
+            if (_end)
+            {
+                return null;
+            }
+            DumpRet ret = await ListPrefix(_bucketName, 
+                Prefix,
+                Marker, 
+                _limit);
+            if (ret.Items.Count == 0)
+            {
+                _end = true;
+                return null;
+            }
+            Marker = ret.Marker;
+            if (Marker == null)
+            {
+                _end = true;
+            }
+            return ret.Items;
+        }
 
-		/// <summary>
-		/// The origin Fetch interface,we recomment to use Next().
-		/// </summary>
-		/// <returns>
-		/// Dump
-		/// </returns>
-		/// <param name='bucketName'>
-		/// Bucket name.
-		/// </param>
-		/// <param name='prefix'>
-		/// Prefix.
-		/// </param>
-		/// <param name='markerIn'>
-		/// Marker in.
-		/// </param>
-		/// <param name='limit'>
-		/// Limit.
-		/// </param>
-		public async System.Threading.Tasks.Task<DumpRet> ListPrefix(string bucketName, string prefix = "", string markerIn = "", int limit = 0)
-		{
-			string url = Config.RSF_HOST + string.Format("/list?bucket={0}", bucketName);// + bucketName + 
-			if (!string.IsNullOrEmpty(markerIn))
-			{
-				url += string.Format("&marker={0}", markerIn);
-			}
-			if (!string.IsNullOrEmpty(prefix))
-			{
-				url += string.Format("&prefix={0}", prefix);
-			}
-			if (limit > 0)
-			{
-				url += string.Format("&limit={0}", limit);
-			}
-			for (int i = 0; i < RETRY_TIME; i++)
-			{
-				CallRet ret = await Call(url);
-				if (ret.OK)
-				{
-					return JsonConvert.DeserializeObject<DumpRet>(ret.Response);
-				}
-				else {
-					continue;
-				}
-			}
-			return null;
-		}
+        private string _bucketName;
 
-		/// <summary>
-		/// call this func before invoke Next()
-		/// </summary>
-		public void Init ()
-		{
-			end = false;
-			this.marker = string.Empty;
-		}
+        private bool _end = false;
 
-		/// <summary>
-		/// Next.
-		/// <example>
-		/// <code>
-		/// public static void List (string bucket)
-		///{
-		///     RSF.RSFClient rsf = new Qiniu.RSF.RSFClient(bucket);
-		///     rsf.Prefix = "test";
-		///     rsf.Limit = 100;
-		///     List<DumpItem> items;
-		///     while ((items=rsf.Next())!=null)
-		///     {                
-		///      //todo
-		///     }
-		///}s
-		/// </code>
-		/// </example>
-		/// </summary>
-		public async System.Threading.Tasks.Task<List<DumpItem>> Next()
-		{
-			if (end)
-			{
-				return null;
-			}
-			try
-			{
-				DumpRet ret = await ListPrefix(this.bucketName, this.prefix, this.marker, this.limit);
-				if (ret.Items.Count == 0)
-				{
-					end = true;
-					return null;
-				}
-				this.marker = ret.Marker;
-				if (this.marker == null)
-					end = true;
-				return ret.Items;
-			}
-			catch (Exception e)
-			{
-				throw e;
-			}
-		}
-	}
+        private int _limit;
+    }
 }
-

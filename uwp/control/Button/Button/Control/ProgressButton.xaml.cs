1a1de305b3be0c800138696a0d135153aad4d536
lindexi_gd <lindexi_gd@163.com>
2016-11-27 20:25:52 +0800

    点下进度条按钮

---
+++ uwp/control/Button/Button/Control/ProgressButton.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Button.Control
+{
+    public sealed partial class ProgressButton : UserControl
+    {
+        public ProgressButton()
+        {
+            InitializeComponent();
+            Enable = true;
+            ProgressVisibility = Visibility.Collapsed;
+        }
+
+        public EventHandler<ProgressButtonEventArgs> ProgressButtonClick
+        {
+            set;
+            get;
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            "Text", typeof(string), typeof(ProgressButton), new PropertyMetadata(default(string)));
+
+        public static readonly DependencyProperty EnableProperty = DependencyProperty.Register(
+            "Enable", typeof(bool), typeof(ProgressButton), new PropertyMetadata(default(bool)));
+
+        public static readonly DependencyProperty ProgressVisibilityProperty = DependencyProperty.Register(
+            "ProgressVisibility", typeof(Visibility), typeof(ProgressButton), new PropertyMetadata(default(Visibility)));
+
+        public Visibility ProgressVisibility
+        {
+            set
+            {
+                SetValue(ProgressVisibilityProperty, value);
+            }
+            get
+            {
+                return (Visibility)GetValue(ProgressVisibilityProperty);
+            }
+        }
+        public bool Enable
+        {
+            set
+            {
+                SetValue(EnableProperty, value);
+            }
+            get
+            {
+                return (bool)GetValue(EnableProperty);
+            }
+        }
+        public string Text
+        {
+            set
+            {
+                SetValue(TextProperty, value);
+            }
+            get
+            {
+                return (string)GetValue(TextProperty);
+            }
+        }
+
+        public void OnFinished()
+        {
+            Enable = true;
+            ProgressVisibility = Visibility.Collapsed;
+        }
+
+        private void ProgressButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (ProgressButtonClick != null)
+            {
+                ProgressButtonClick.Invoke(sender, new ProgressButtonEventArgs(OnFinished,e));
+                Enable = false;
+                ProgressVisibility = Visibility.Visible;
+            }
+        }
+
+        public class ProgressButtonEventArgs : EventArgs
+        {
+            public ProgressButtonEventArgs(Action onFinished, RoutedEventArgs e)
+            {
+                OnFinished = onFinished;
+                OriginalSource = e;
+            }
+
+            public RoutedEventArgs OriginalSource
+            {
+                get;
+            }
+
+            public Action OnFinished
+            {
+                get;
+            }
+        }
+    }
+}
--- uwp/control/Button/Button/MainPage.xaml.cs
+++ uwp/control/Button/Button/MainPage.xaml.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,6 +15,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Button.Control;
 
 //“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
 
@@ -25,6 +29,41 @@ namespace Button
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void ProgressButton_OnClick(object sender, ProgressButton.ProgressButtonEventArgs e)
+        {
+            await Task.Delay(1000);
+            e.OnFinished.Invoke();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+
+            Data data = new Data()
+            {
+                Name = "2"
+            };
+
+            var serializer = new DataContractJsonSerializer(typeof(Data));
+
+            string str = null;
+            //StringBuilder str=new StringBuilder();
+            Stream stream = new MemoryStream();
+            serializer.WriteObject(stream, data);
+            str = new StreamReader(stream).ReadToEnd();
+            //StreamWriter stream=new StreamWriter();
+        }
+
+        private class Data
+        {
+            
+            public string Name
+            {
+                set;
+                get;
+            }
+
         }
     }
 }

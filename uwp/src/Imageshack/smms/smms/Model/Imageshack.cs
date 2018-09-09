0fc1f5b3a219b4c19c711adebadcb0b5ebafda4e
lindexi_gd <lindexi_gd@163.com>
2018-09-09 10:53:19 +0800

    添加让开发者可以上传图片

--- uwp/src/Imageshack/smms/smms/Model/Imageshack.cs
+++ uwp/src/Imageshack/smms/smms/Model/Imageshack.cs
@@ -7,11 +7,12 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Storage;
 using Windows.Web.Http;
-using HttpClient = Windows.Web.Http.HttpClient;
+using HttpClient = System.Net.Http.HttpClient;
 
 namespace smms.Model
 {
@@ -50,16 +51,28 @@ namespace smms.Model
         public async void UpLoad()
         {
             string url = "https://sm.ms/api/upload";
-            HttpClient webHttpClient =
+            HttpClient httpClient =
                 new HttpClient();
-            HttpMultipartFormDataContent httpMultipartFormDataContent =
-                new HttpMultipartFormDataContent();
-            var fileContent = new HttpStreamContent(await File.OpenAsync(FileAccessMode.Read));
-            fileContent.Headers.Add("Content-Type", "application/octet-stream");
-            httpMultipartFormDataContent.Add(fileContent, "smfile", File.Name);
-            var str = await webHttpClient.PostAsync(new Uri(url), httpMultipartFormDataContent);
-            ResponseString = str.Content.ToString();
-            OnUploadedEventHandler?.Invoke(this, ResponseString);
+
+            var multipartFormDataContent = new MultipartFormDataContent();
+            var fileContent = new StreamContent((await File.OpenAsync(FileAccessMode.Read)).AsStream());
+            fileContent.Headers.ContentType=new MediaTypeHeaderValue("application/octet-stream");
+            multipartFormDataContent.Add(fileContent,"smfile",File.Name);
+            var userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36";
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(userAgent);
+            var response =await httpClient.PostAsync(new Uri(url),multipartFormDataContent);
+            var str = await response.Content.ReadAsStringAsync();
+
+            //HttpMultipartFormDataContent httpMultipartFormDataContent =
+            //    new HttpMultipartFormDataContent();
+            //var fileContent = new HttpStreamContent(await File.OpenAsync(FileAccessMode.Read));
+            //fileContent.Headers.Add("Content-Type", "application/octet-stream");
+            //httpMultipartFormDataContent.Add(fileContent, "smfile", File.Name);
+
+
+            //var str = await webHttpClient.PostAsync(new Uri(url), httpMultipartFormDataContent);
+            //ResponseString = str.Content.ToString();
+            //OnUploadedEventHandler?.Invoke(this, ResponseString);
         }
     }
 }
---
+++ uwp/src/Imageshack/smms/smms/Model/SmmsImageshack.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Web.Http;
+using Newtonsoft.Json;
+
+namespace lindexi.Imageshack.Smms
+{
+    /// <summary>
+    /// Sm.ms 图床帮助类，提供上传图片到 Smms 图床
+    /// </summary>
+    public class SmmsImageshack
+    {
+        /// <summary>
+        /// 需要上传的图片文件
+        /// </summary>
+        /// <param name="storageFile"></param>
+        public SmmsImageshack(StorageFile storageFile)
+        {
+            StorageFile = storageFile;
+        }
+
+        /// <summary>
+        /// 上传图片到 smms 网站
+        /// </summary>
+        /// <exception cref="IOException"></exception>
+        /// <exception cref="COMException">超时</exception>
+        /// <returns></returns>
+        public async Task<Smms> UploadImage()
+        {
+            const string url = "https://sm.ms/api/upload";
+            var webHttpClient =
+                new HttpClient();
+            var httpMultipartFormDataContent =
+                new HttpMultipartFormDataContent();
+
+            var fileContent = new HttpStreamContent(await StorageFile.OpenAsync(FileAccessMode.Read));
+
+            fileContent.Headers.Add("Content-Type", "application/octet-stream");
+
+            // 更多 userAgent 请看 win10 uwp 如何让WebView标识win10手机 https://lindexi.gitee.io/post/win10-uwp-%E5%A6%82%E4%BD%95%E8%AE%A9WebView%E6%A0%87%E8%AF%86win10%E6%89%8B%E6%9C%BA.html
+            var userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36";
+
+            webHttpClient.DefaultRequestHeaders.UserAgent.ParseAdd(userAgent);
+
+
+            httpMultipartFormDataContent.Add(fileContent, "smfile", StorageFile.Name);
+
+            var response = await webHttpClient.PostAsync(new Uri(url), httpMultipartFormDataContent);
+
+            var responseString = response.Content.ToString();
+
+            Smms smms = JsonConvert.DeserializeObject<Smms>(responseString);
+            return smms;
+        }
+
+        private StorageFile StorageFile { get; }
+
+        public class Smms
+        {
+            [JsonProperty(propertyName: "code")]
+            public string code { get; set; }
+            [JsonProperty(propertyName: "data")]
+            public Data data { get; set; }
+        }
+
+        public class Data
+        {
+            [JsonProperty(propertyName: "width")]
+            public int width { get; set; }
+            [JsonProperty(propertyName: "height")]
+            public int height { get; set; }
+            [JsonProperty(propertyName: "filename")]
+            public string filename { get; set; }
+            [JsonProperty(propertyName: "storename")]
+            public string storename { get; set; }
+            [JsonProperty(propertyName: "size")]
+            public int size { get; set; }
+            [JsonProperty(propertyName: "path")]
+            public string path { get; set; }
+            [JsonProperty(propertyName: "hash")]
+            public string hash { get; set; }
+            [JsonProperty(propertyName: "timestamp")]
+            public int timestamp { get; set; }
+            [JsonProperty(propertyName: "ip")]
+            public string ip { get; set; }
+            [JsonProperty(propertyName: "url")]
+            public string url { get; set; }
+            [JsonProperty(propertyName: "delete")]
+            public string delete { get; set; }
+        }
+    }
+}

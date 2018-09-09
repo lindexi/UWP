3fee288b05a1992dae5ad0d448a9142bf7a4f29e
lindexi_gd <lindexi_gd@163.com>
2018-09-09 11:10:44 +0800

    修改变量名

--- uwp/src/Imageshack/smms/lindexi.Imageshack.Smms/SmmsImageshack.cs
+++ uwp/src/Imageshack/smms/lindexi.Imageshack.Smms/SmmsImageshack.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Windows.Storage;
-using Windows.Web.Http;
 using Newtonsoft.Json;
 
 namespace lindexi.Imageshack.Smms
@@ -19,8 +20,24 @@ namespace lindexi.Imageshack.Smms
         /// <param name="storageFile"></param>
         public SmmsImageshack(StorageFile storageFile)
         {
-            StorageFile = storageFile;
+            StorageFile = storageFile ?? throw new ArgumentNullException(nameof(storageFile));
         }
+
+        /// <summary>
+        /// 需要上传的图片文件
+        /// </summary>
+        public SmmsImageshack(string fileName, Stream file)
+        {
+            _stream = file ?? throw new ArgumentNullException(nameof(file));
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+
+            _fileName = fileName;
+        }
+
 
         /// <summary>
         /// 上传图片到 smms 网站
@@ -31,65 +48,121 @@ namespace lindexi.Imageshack.Smms
         /// <returns></returns>
         public async Task<Smms> UploadImage()
         {
+            if (StorageFile != null)
+            {
+                var stream = (await StorageFile.OpenAsync(FileAccessMode.Read)).AsStream();
+                var fileName = StorageFile.Name;
+                return await UploadImageInternal(stream, fileName);
+            }
+            else
+            {
+                return await UploadImageInternal(_stream, _fileName);
+            }
+        }
+
+        private async Task<Smms> UploadImageInternal(Stream file, string fileName)
+        {
             const string url = "https://sm.ms/api/upload";
-            var webHttpClient =
+            var httpClient =
                 new HttpClient();
-            var httpMultipartFormDataContent =
-                new HttpMultipartFormDataContent();
+            var userAgent =
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36";
 
-            var fileContent = new HttpStreamContent(await StorageFile.OpenAsync(FileAccessMode.Read));
-            fileContent.Headers.Add("Content-Type", "application/octet-stream");
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(userAgent);
 
-            // 更多 userAgent 请看 win10 uwp 如何让WebView标识win10手机 https://lindexi.gitee.io/post/win10-uwp-%E5%A6%82%E4%BD%95%E8%AE%A9WebView%E6%A0%87%E8%AF%86win10%E6%89%8B%E6%9C%BA.html
-            var userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36";
+            await httpClient.PostAsync(new Uri(url), new StringContent(""));
 
-            webHttpClient.DefaultRequestHeaders.UserAgent.ParseAdd(userAgent);
+            var multipartFormDataContent = new MultipartFormDataContent();
+            var fileContent = new StreamContent(file);
+            fileContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            multipartFormDataContent.Add(fileContent, "smfile", fileName);
 
+            var response = await httpClient.PostAsync(new Uri(url), multipartFormDataContent);
+            var str = await response.Content.ReadAsStringAsync();
 
-            httpMultipartFormDataContent.Add(fileContent, "smfile", StorageFile.Name);
-
-            var response = await webHttpClient.PostAsync(new Uri(url), httpMultipartFormDataContent);
-
-            var responseString = response.Content.ToString();
-
-            Smms smms = JsonConvert.DeserializeObject<Smms>(responseString);
+            var smms = JsonConvert.DeserializeObject<Smms>(str);
             return smms;
         }
 
         private StorageFile StorageFile { get; }
 
+        private readonly Stream _stream;
+        private readonly string _fileName;
+
+
         public class Smms
         {
+            /// <summary>
+            /// 上传文件状态。正常情况为 success。出现错误时为 error
+            /// </summary>
             [JsonProperty(propertyName: "code")]
-            public string code { get; set; }
+            public string Code { get; set; }
+
             [JsonProperty(propertyName: "data")]
-            public Data data { get; set; }
+            public Data Data { get; set; }
         }
 
         public class Data
         {
+            /// <summary>
+            /// 图片的宽度
+            /// </summary>
             [JsonProperty(propertyName: "width")]
-            public int width { get; set; }
+            public int Width { get; set; }
+
+            /// <summary>
+            /// 图片的高度
+            /// </summary>
             [JsonProperty(propertyName: "height")]
-            public int height { get; set; }
+            public int Height { get; set; }
+
+            /// <summary>
+            /// 上传文件时所用的文件名
+            /// </summary>
             [JsonProperty(propertyName: "filename")]
-            public string filename { get; set; }
+            public string Filename { get; set; }
+
+            /// <summary>
+            /// 上传后的文件名
+            /// </summary>
             [JsonProperty(propertyName: "storename")]
-            public string storename { get; set; }
+            public string Storename { get; set; }
+
+            /// <summary>
+            /// 文件大小
+            /// </summary>
             [JsonProperty(propertyName: "size")]
-            public int size { get; set; }
+            public int Size { get; set; }
+
+            /// <summary>
+            /// 图片的相对地址
+            /// </summary>
             [JsonProperty(propertyName: "path")]
-            public string path { get; set; }
+            public string Path { get; set; }
+
+            /// <summary>
+            /// 随机字符串，用于删除文件
+            /// </summary>
             [JsonProperty(propertyName: "hash")]
-            public string hash { get; set; }
+            public string Hash { get; set; }
+
             [JsonProperty(propertyName: "timestamp")]
-            public int timestamp { get; set; }
+            public int Timestamp { get; set; }
+
             [JsonProperty(propertyName: "ip")]
-            public string ip { get; set; }
+            public string Ip { get; set; }
+
+            /// <summary>
+            /// 图片服务器地址
+            /// </summary>
             [JsonProperty(propertyName: "url")]
-            public string url { get; set; }
+            public string Url { get; set; }
+
+            /// <summary>
+            /// 删除上传的图片文件专有链接
+            /// </summary>
             [JsonProperty(propertyName: "delete")]
-            public string delete { get; set; }
+            public string Delete { get; set; }
         }
     }
 }

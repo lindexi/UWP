cc8ab8c3fd0a4455e940aa20e394a8083eb93f06
lindexi <lindexi_gd@163.com>
2018-09-20 20:50:55 +0800

    反射组合提高开发速度

---
+++ uwp/src/Framework/UWP/ReflectionCombineViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using lindexi.MVVM.Framework.Annotations;
+using lindexi.MVVM.Framework.ViewModel;
+
+namespace lindexi.uwp.Framework
+{
+    /// <summary>
+    /// 反射组合 ViewModel 和页面
+    /// </summary>
+    public static class ReflectionCombineViewModel
+    {
+        /// <summary>
+        /// 组合 ViewModel 和页面，调用这个方法会读取输入程序集的所有 ViewModel 找到标记对应  <see cref="ViewModelAttribute"/> 的页面
+        /// </summary>
+        /// <param name="viewModel"></param>
+        /// <param name="assembly"></param>
+        public static void CombineViewModel([NotNull] this NavigateViewModel viewModel, [NotNull] Assembly assembly)
+        {
+            if (ReferenceEquals(viewModel, null)) throw new ArgumentNullException(nameof(viewModel));
+
+            if (ReferenceEquals(assembly, null)) throw new ArgumentNullException(nameof(assembly));
+
+            if (viewModel.ViewModelPage == null)
+            {
+                viewModel.ViewModelPage = new List<ViewModelPage>();
+            }
+
+            viewModel.ViewModelPage.Clear();
+
+            foreach (var temp in assembly.DefinedTypes.Where(temp => typeof(IViewModel).IsAssignableFrom(temp.AsType()))
+            )
+            {
+                viewModel.ViewModelPage.Add(new ViewModelPage(new NavigatableViewModel(temp)));
+            }
+
+            foreach (var temp in assembly.DefinedTypes.Where(temp =>
+                temp.IsSubclassOf(typeof(Windows.UI.Xaml.Controls.Page))))
+            {
+                var p = temp.GetCustomAttribute<ViewModelAttribute>();
+                if (p != null)
+                {
+                    var viewmodel =
+                        viewModel.ViewModelPage.FirstOrDefault(t => t.ViewModel.Name.Equals(p.ViewModel.Name));
+
+                    if (viewmodel != null)
+                    {
+                        viewModel.ViewModelPage.Remove(viewmodel);
+                        viewModel.ViewModelPage.Add(new ViewModelPage(viewmodel.ViewModel, new NavigatablePage(temp)));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ uwp/src/Framework/WPF/ReflectionCombineViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lindexi.wpf.Framework
+{
+    /// <summary>
+    ///  反射组合 ViewModel 和页面
+    /// </summary>
+    internal class ReflectionCombineViewModel
+    {
+    }
+}

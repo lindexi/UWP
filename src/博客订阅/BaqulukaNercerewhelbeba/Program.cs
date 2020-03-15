a003bc7aceaa8c20bda82ecd47eaa6d534bc8ac0
林德熙 <lindexi@cvte.com>
2020-03-15 15:52:49 +0800

    修复域名

--- src/博客订阅/BaqulukaNercerewhelbeba/Program.cs
+++ src/博客订阅/BaqulukaNercerewhelbeba/Program.cs
@@ -31,6 +31,7 @@ namespace BaqulukaNercerewhelbeba
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://0.0.0.0:80");
                 });
     }
 

f4e0dcf52dfff015363664e8bc740086f5c4c12e
lindexi <lindexi_gd@163.com>
2020-10-16 08:58:16 +0800

    加上对称图形控件

---
+++ wpf/WPFFlipDrawingCanvas/WPFFlipDrawingCanvas/FourGridFlipDrawingControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Lindexi.Control.WPFFlipDrawingCanvas
+{
+    /// <summary>
+    /// FourGridFlipDrawingControl.xaml 的交互逻辑
+    /// </summary>
+    internal partial class FourGridFlipDrawingControl : UserControl
+    {
+        public FourGridFlipDrawingControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ wpf/WPFFlipDrawingCanvas/WPFFlipDrawingCanvas/VisualCanvas.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Lindexi.Control.WPFFlipDrawingCanvas
+{
+    internal class VisualCanvas : FrameworkElement
+    {
+        protected override Visual GetVisualChild(int index)
+        {
+            return Visual;
+        }
+
+        protected override int VisualChildrenCount => 1;
+
+        public VisualCanvas(DrawingVisual visual)
+        {
+            Visual = visual;
+            AddVisualChild(visual);
+        }
+
+        public DrawingVisual Visual { get; }
+    }
+}

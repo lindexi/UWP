baaf7aae0168002886c7e561de0231c02c57e90a
lindexi <lindexi_gd@163.com>
2021-01-13 20:54:00 +0800

    测试加上服务

--- app/OTAManager/demo/OTAManager.Demo.WPF/App.xaml.cs
+++ app/OTAManager/demo/OTAManager.Demo.WPF/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.Hosting;
 
 namespace OTAManager.Demo.WPF
 {
@@ -13,5 +14,20 @@ namespace OTAManager.Demo.WPF
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // 启动主机调试
+            Task.Run(() =>
+            {
+                var host = OTAManager.Server.Program.CreateHostBuilder(e.Args).Build();
+                Host = host;
+
+                host.Run();
+            });
+            
+            base.OnStartup(e);
+        }
+
+        public static IHost Host { private set; get; }
     }
 }

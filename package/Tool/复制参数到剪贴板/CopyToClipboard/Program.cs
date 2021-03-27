a800129fa51ca98af73bfb61aa75de7a601f2c45
lindexi <lindexi_gd@163.com>
2021-03-27 14:36:40 +0800

    复制参数到剪贴板

---
+++ package/Tool/复制参数到剪贴板/CopyToClipboard/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace Lindexi.Tool.CopyToClipboard
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var commandLine = string.Join(" ", args);
+            if (string.IsNullOrEmpty(commandLine))
+            {
+                Console.WriteLine("啥都没有");
+            }
+            else
+            {
+                Clipboard.SetText(commandLine);
+                Console.WriteLine($"已复制{commandLine}到剪贴板");
+            }
+        }
+    }
+}

f4f12ccb20887361e103db021e983cba3a5d120b
lindexi <lindexi_gd@163.com>
2024-09-10 14:36:48 +0800

    定义代码辅助类

---
+++ src/Common/Core/IO/CodeSolutionHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace Lindexi.Src.Core.IO;
+
+public static class CodeSolutionHelper
+{
+    public static string? GetCodeSolutionPath(string? currentPath = null)
+    {
+        currentPath ??= Directory.GetCurrentDirectory();
+
+        while (currentPath != null)
+        {
+            var files = Directory.EnumerateFiles(currentPath, "*.sln");
+            if (files.Any())
+            {
+                return currentPath;
+            }
+
+            currentPath = Path.GetDirectoryName(currentPath);
+        }
+
+        return null;
+    }
+}

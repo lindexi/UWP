d6bb9b0faa58d6507a56a2bf2031994b0c6fc641
lindexi <lindexi_gd@163.com>
2025-03-14 09:55:43 +0800

    添加屏幕信息展示

---
+++ app/Tool/屏幕信息展示/KeyboardHookListener.cs
@@ -0,0 +1,101 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
+
+namespace DacemcalqeleHalibarbubem;
+
+public static class KeyboardHookListener
+{
+    public static void HookKeyboard()
+    {
+        if (_proc != null) return;
+        _proc = HookCallback;
+        ////安装当前线程的钩子
+        //_threadHookId =
+        //    InterceptKeys.SetHook(_proc, InterceptKeys.WH_KEYBOARD, GetCurrentThreadId());
+        //安装全局的钩子
+        _globalHookId = SetHook(_proc, WINDOWS_HOOK_ID.WH_KEYBOARD_LL, 0);
+    }
+
+    /// <summary>
+    /// 卸载钩子
+    /// </summary>
+    public static void UnhookKeyboard()
+    {
+        //if (_threadHookId != IntPtr.Zero)
+        //{
+        //    PInvoke.UnhookWindowsHookEx(new HHOOK(_threadHookId));
+        //}
+
+        if (!_globalHookId.IsInvalid)
+        {
+            PInvoke.UnhookWindowsHookEx(new HHOOK(_globalHookId.DangerousGetHandle()));
+            _globalHookId.SetHandleAsInvalid();
+        }
+
+        _proc = null;
+    }
+
+    public static event RawKeyEventHandler? KeyDown;
+    public static event RawKeyEventHandler? KeyUp;
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static LRESULT HookCallback(int code, WPARAM wparam, LPARAM lparam)
+    {
+        if (wparam == PInvoke.WM_KEYDOWN)
+        {
+            int vkCode = Marshal.ReadInt32(lparam);
+
+            if (KeyDown != null)
+                KeyDown(null, new RawKeyEventArgs(vkCode, false));
+        }
+        else if (wparam == PInvoke.WM_KEYUP)
+        {
+            int vkCode = Marshal.ReadInt32(lparam);
+
+            if (KeyUp != null)
+            {
+                KeyUp(null, new RawKeyEventArgs(vkCode, false));
+            }
+        }
+
+        return PInvoke.CallNextHookEx(_globalHookId, code, wparam, lparam);
+    }
+
+    /// <summary>
+    /// 定义一个局部变量保存，防止被垃圾回收
+    /// </summary>
+    private static HOOKPROC? _proc;
+    public delegate void RawKeyEventHandler(object? sender, RawKeyEventArgs args);
+    public class RawKeyEventArgs : EventArgs
+    {
+        public int VKCode { get; }
+        public Key Key { get; }
+        public bool IsSysKey { get; }
+
+        public RawKeyEventArgs(int VKCode, bool isSysKey)
+        {
+            this.VKCode = VKCode;
+            this.IsSysKey = isSysKey;
+            this.Key = System.Windows.Input.KeyInterop.KeyFromVirtualKey(VKCode);
+        }
+    }
+
+    //private static IntPtr _threadHookId = IntPtr.Zero;
+    private static UnhookWindowsHookExSafeHandle _globalHookId = new UnhookWindowsHookExSafeHandle(IntPtr.Zero, false);
+
+    private static UnhookWindowsHookExSafeHandle SetHook(HOOKPROC proc, WINDOWS_HOOK_ID hookType, uint threadId)
+    {
+        using var currentProcess = Process.GetCurrentProcess();
+        var currentProcessMainModule = currentProcess.MainModule!;
+
+        var moduleHandle = PInvoke.GetModuleHandle(currentProcessMainModule.ModuleName);
+
+        return PInvoke.SetWindowsHookEx(hookType, proc, moduleHandle, threadId);
+    }
+}
---
+++ app/Tool/屏幕信息展示/MainWindow.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Windows.Win32;
+using dotnetCampus.Configurations;
+using dotnetCampus.Configurations.Core;
+using System.Text.RegularExpressions;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+namespace DacemcalqeleHalibarbubem;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        KeyboardHookListener.KeyDown += KeyboardHookListener_KeyDown;
+        KeyboardHookListener.KeyUp += KeyboardHookListener_KeyUp;
+        KeyboardHookListener.HookKeyboard();
+
+        Loaded += MainWindow_Loaded;
+
+        AllowsTransparency = true;
+        Opacity = 0.65;
+
+        var file = @"C:\lindexi\Application\ApplicationConfiguration\屏幕信息展示\Configuration.coin";
+        var fileConfigurationRepo = ConfigurationFactory.FromFile(file);
+        _fileConfigurationRepo = fileConfigurationRepo;
+        var appConfigurator = fileConfigurationRepo.CreateAppConfigurator();
+        AppConfigurator = appConfigurator;
+
+        Closed += MainWindow_Closed;
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void MainWindow_Closed(object? sender, EventArgs e)
+    {
+        _ = _fileConfigurationRepo.SaveAsync();
+    }
+
+    private readonly Stopwatch _lastCtrlKeyDown = new Stopwatch();
+
+    private void KeyboardHookListener_KeyDown(object? sender, KeyboardHookListener.RawKeyEventArgs args)
+    {
+        if (args.Key == Key.LWin)
+        {
+            ShowNotActive();
+        }
+    }
+
+    private async void ShowNotActive()
+    {
+        Show();
+        Topmost = true;
+        Topmost = false;
+
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        try
+        {
+            using var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Referer", "https://finance.sina.com.cn/");
+
+            var response = await httpClient.GetAsync($"https://hq.sinajs.cn/list={AppConfigurator.Of<DacemcalqeleHalibarbubemConfiguratioin>().TickName}");
+            var stream = await response.Content.ReadAsStreamAsync();
+            var streamReader = new StreamReader(stream, Encoding.GetEncoding("GBK"));
+            var text = await streamReader.ReadToEndAsync();
+
+            var match = Regex.Match(text,
+                @"var hq_str_\w+=""(\w+),([\d\.]+),([\d\.]+),([\d\.]+),([\d\.]+),([\d\.]+),([\d\.]+),([\d\.]+),([\d\.]+),([\d\.]+),");
+            var current = match.Groups[4].Value;
+            var max = match.Groups[5].Value;
+            var min = match.Groups[6].Value;
+            MessageTextBox.Text = $"{current} {max} {min}\r\n{DateTime.Now}";
+        }
+        catch
+        {
+            // 忽略
+        }
+        _ = TryHide();
+    }
+
+    private void KeyboardHookListener_KeyUp(object? sender, KeyboardHookListener.RawKeyEventArgs args)
+    {
+        if (args.Key == Key.LeftCtrl)
+        {
+            if (_lastCtrlKeyDown.IsRunning && _lastCtrlKeyDown.Elapsed < TimeSpan.FromMilliseconds(500))
+            {
+                ShowNotActive();
+            }
+            else
+            {
+                _lastCtrlKeyDown.Restart();
+            }
+        }
+    }
+
+    private ulong _hideVersion;
+    private readonly FileConfigurationRepo _fileConfigurationRepo;
+    public IAppConfigurator AppConfigurator { get; set; }
+
+    private async Task TryHide()
+    {
+        _hideVersion++;
+        var version = _hideVersion;
+        await Task.Delay(TimeSpan.FromSeconds(10));
+        if (version == _hideVersion)
+        {
+            Hide();
+        }
+    }
+
+    protected override void OnPreviewMouseMove(MouseEventArgs e)
+    {
+        _ = TryHide();
+    }
+
+    protected override void OnMouseDown(MouseButtonEventArgs e)
+    {
+        DragMove();
+    }
+
+    class DacemcalqeleHalibarbubemConfiguratioin : Configuration
+    {
+        public DacemcalqeleHalibarbubemConfiguratioin() : base("")
+        {
+        }
+
+        public string TickName
+        {
+            get => GetString() ?? "sz000651";
+        }
+    }
+}

6de075ca68752e11238f9060d3783b61540d238c
lindexi <lindexi_gd@163.com>
2026-02-24 11:00:55 +0800

    提供远程调用的写法

---
+++ src/RemoteExecutor/Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+using RemoteExecutors;
+
+if (RemoteExecutor.TryHandle(args))
+{
+    return;
+}
+
+RemoteExecutor.Invoke(() =>
+{
+    // 写个文件测试一下
+    var file = Path.Join(AppContext.BaseDirectory, "1.txt");
+    File.WriteAllText(file, DateTime.Now.ToString(CultureInfo.InvariantCulture));
+});
+
+Console.WriteLine("Hello, World!");
